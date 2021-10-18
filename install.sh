b4f723bb961de7c23fe34ff306a9aadc4db62acd
Niyabi <kufel92@gmail.com>
2021-10-18 14:51:04 +0200

    Fish plugins, readme

--- install.sh
+++ install.sh
@@ -19,3 +19,41 @@ apt update
 sudo apt install fish
 chsh -s /usr/bin/fish
 
+# Install fisher, fish plugin manager
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+# Install fish Node Version Manager
+fisher install jorgebucaran/nvm.fish
+
+# Install peco for fish
+fisher takashabe/fish-peco
+
+# Install z for fish
+fisher install jethrokuan/z
+
+# Install Silversearcher
+apt install -y silversearcher-ag
+
+# Install Tide theme for fish
+fisher install IlanCosman/tide@v5
+
+# Write fish settings to config.fish
+cd ~/.config/fish/
+cat <<EOF > config.fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+  set fish_greeting ""
+  alias inst "sudo apt install -y"
+  alias dc "docker-compose"
+  alias dcb "dc exec fpm bash"
+  alias ci "code-insiders ."
+  alias vsc "code ."
+
+  if type -q exa
+    alias ll "exa -l -g --icons"
+    alias lla "ll -a"
+  end
+
+end
+EOF
